419b65707590c7d09462056cfcb479de90307b05
Musiquammation <bryanmegherbi@gmail.com>
2026-03-02 22:56:25 +0100

    Replace cyan by green

--- public/bundle.js
+++ public/bundle.js
@@ -2577,7 +2577,7 @@ const LEVELS = [
       {
         x: 1,
         y: 11,
-        color: CarColor.CYAN,
+        color: CarColor.GREEN,
         rythm: 60,
         couldown: 1,
         direction: Direction.RIGHT,
@@ -2617,7 +2617,7 @@ const LEVELS = [
       {
         x: 1,
         y: 27,
-        color: CarColor.CYAN,
+        color: CarColor.GREEN,
         rythm: 60,
         couldown: 1,
         direction: Direction.RIGHT,
@@ -2705,7 +2705,7 @@ const LEVELS = [
       { x: 28, y: 2, data: roadtypes.types.CONSUMER | CarColor.RED << 3 },
       { x: 28, y: 9, data: roadtypes.types.CONSUMER | CarColor.YELLOW << 3 },
       { x: 28, y: 23, data: roadtypes.types.CONSUMER | CarColor.BLUE << 3 },
-      { x: 28, y: 30, data: roadtypes.types.CONSUMER | CarColor.CYAN << 3 },
+      { x: 28, y: 30, data: roadtypes.types.CONSUMER | CarColor.GREEN << 3 },
       { x: 1, y: 17, data: roadtypes.types.CONSUMER | CarColor.PINK << 3 }
     ]
   })
